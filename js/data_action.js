51e93ad37bb8579a3aa4a19e85b002a9f76a6d87
Noah Ehrman <giantnoah@gmail.com>
2020-07-07 23:08:06 -0400

    Added Details and icons for gun attacks

--- js/data_action.js
+++ js/data_action.js
@@ -1,13 +1,53 @@
 data_action = [
     {
         title: "Single Fire",
-        icon: "crossed-swords",
-        subtitle: "Common Ranged Attack",
-        description: "Fire a single bullet from your weapon",
-        reference: "Core, pg. 105-105",
+        icon: "gunshot",
+        subtitle: "Ranged attack",
+        description: "Fire a single shot from your weapon",
+        reference: "Core, pg. 105-105.",
         bullets: [
-		"Roll Ref + Weapon Skill + Weapon Accuracy + 1d10 vs Hit Numbers Table",
-		"You may only fire up to your weapon's maximum Rate of Fire (ROF) in a single turn"
+		"Roll: 1d10 + Ref + Weapon Skill + Weapon Accuracy",
+		"Fumbles if the d10 comes up as a 1, automatically missing",
+		"If the result is greater than the appropriate range DC then the shot hits"
+        ]
+    },
+    {
+        title: "Three Round Burst",
+        icon: "bullets",
+        subtitle: "3-shot ranged attack",
+        description: "Fire three shots from your weapon",
+        reference: "Core, pg. 106.",
+        bullets: [
+		"Roll: Ref + Weapon Skill + Weapon Accuracy + 1d10",
+		"Add +3 to your roll if rolling in medium or close range",
+		"If the result is greater than the appropriate range DC then the shot hits",
+		"On a hit, roll 1d6/2 (rounding up) to determine how many shots hit"
+        ]
+    },
+    {
+        title: "Full Auto",
+        icon: "machine-gun",
+        subtitle: "Several shot ranged attack",
+        description: "Fire up to your weapon's total rate of fire at one or multiple targets",
+        reference: "Core, pg. 106.",
+        bullets: [
+		"Roll: Ref + Weapon Skill + Weapon Accuracy + 1d10 for each target",
+		"Add +1 to your roll for every 10 shots fired in close range",
+		"Subtract -1 from your roll for every 10 shots fired in medium, long, and extreme range",
+		"For every point of success over the required hit roll, one round hits the target up to the maximum ROF for the weapon"
+        ]
+    },
+    {
+        title: "Grapple",
+        icon: "grab",
+        subtitle: "Special melee attack",
+        description: "Attempt to grab a creature or wrestle with it",
+        reference: "PHB, pg. 195.",
+        bullets: [
+            "You can use the <i>Attack</i> action to make a special melee attack, a grapple. If you're able to make multiple attacks with the Attack action, this attack replaces one of them.",
+            "The target of your grapple must be no more than one size larger than you, and it must be within your reach.",
+            "Using at least one free hand, you try to seize the target by making a grapple check, a Strength (Athletics) check contested by the target's Strength (Athletics) or Dexterity (Acrobatics) check (the target chooses the ability to use).",
+            "If you succeed, you subject the target to the grappled condition (its speed is set to 0)."
         ]
     },
     {
