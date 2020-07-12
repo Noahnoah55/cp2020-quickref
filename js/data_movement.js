879767e969691802730ca2c6323a473993f011ec
Noah Ehrman <giantnoah@gmail.com>
2020-07-11 21:57:47 -0400

    Finished movement rules from core

--- js/data_movement.js
+++ js/data_movement.js
@@ -6,17 +6,19 @@ data_movement = [
         description: "Cost: 1 MA per 3 Meters",
         reference: "Core, pg. 26.",
         bullets: [
-		"Move up to 3x your MA in meters"
+		"Move up to 3x your MA in meters",
+		"Listed as RUN on your character sheet"
         ]
     },
     {
         title: "Leap",
-        icon: "crags",
-        subtitle: "Cost: 10ft per 5ft",
+        icon: "wingfoot",
+        subtitle: "Cost: 4 Meters per 1 Meter",
         description: "Movement cost: 10ft per 5ft climbed",
-        reference: "PHB, pg. 182.",
+        reference: "Core, pg. 26.",
         bullets: [
-            "May involve a Strength (Athletics) check if the climb is difficult"
+		"Leap up to 3/4 your MA in meters from a running start",
+		"Listed as LEAP on your character sheet"
         ]
     }
 ]
