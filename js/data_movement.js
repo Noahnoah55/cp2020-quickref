195e9630d828a4d32892215a20b82024d845f123
Noah Ehrman <giantnoah@gmail.com>
2020-07-24 18:14:10 -0400

    Fixed indendation in movement and working in index

--- js/data_movement.js
+++ js/data_movement.js
@@ -1,25 +1,25 @@
 data_movement = [
-    {
-        title: "Move",
-        icon: "run",
-        subtitle: "Cost: 1 MA per 3 meters",
-        description: "Cost: 1 MA per 3 meters",
-        reference: "Core, pg. 26.",
-        bullets: [
-		"Move up to 3x your MA in meters",
-		"Listed as RUN on your character sheet"
-        ]
-    },
-    {
-        title: "Leap",
-        icon: "wingfoot",
-        subtitle: "Leap up to 3/4 your MA in meters",
-        description: "Leap up to 3/4 you MA in meters",
-        reference: "Core, pg. 26.",
-        bullets: [
-		"Leap up to 3/4 your MA in meters from a running start",
-		"Listed as LEAP on your character sheet",
-		"The referee may require an Athletics (REF) check for difficult jumps"
-        ]
-    }
+	{
+		title: "Move",
+		icon: "run",
+		subtitle: "Cost: 1 MA per 3 meters",
+		description: "Cost: 1 MA per 3 meters",
+		reference: "Core, pg. 26.",
+		bullets: [
+			"Move up to 3x your MA in meters per round",
+			"The maximum distance you can run per round is listed as RUN on your character sheet"
+		]
+	},
+	{
+		title: "Leap",
+		icon: "wingfoot",
+		subtitle: "Leap up to 3/4 your MA in meters",
+		description: "Leap up to 3/4 you MA in meters"
+		reference: "Core, pg. 26.",
+		bullets: [
+			"Leap up to 3/4 your MA in meters from a running start",
+			"Listed as LEAP on your character sheet",
+			"The referee may require an Athletics (REF) check for difficult jumps"
+		]
+	}
 ]
