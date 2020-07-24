6103b980f01ec25655b364a74d49a4d9ef507fcd
Noah Ehrman <giantnoah@gmail.com>
2020-07-24 18:05:48 -0400

    Clarified leap rules

--- js/data_movement.js
+++ js/data_movement.js
@@ -2,8 +2,8 @@ data_movement = [
     {
         title: "Move",
         icon: "run",
-        subtitle: "Cost: 1 MA per 3 Meters",
-        description: "Cost: 1 MA per 3 Meters",
+        subtitle: "Cost: 1 MA per 3 meters",
+        description: "Cost: 1 MA per 3 meters",
         reference: "Core, pg. 26.",
         bullets: [
 		"Move up to 3x your MA in meters",
@@ -13,12 +13,13 @@ data_movement = [
     {
         title: "Leap",
         icon: "wingfoot",
-        subtitle: "Cost: 4 Meters per 1 Meter",
-        description: "Movement cost: 10ft per 5ft climbed",
+        subtitle: "Leap up to 3/4 your MA in meters",
+        description: "Leap up to 3/4 you MA in meters",
         reference: "Core, pg. 26.",
         bullets: [
 		"Leap up to 3/4 your MA in meters from a running start",
-		"Listed as LEAP on your character sheet"
+		"Listed as LEAP on your character sheet",
+		"The referee may require an Athletics (REF) check for difficult jumps"
         ]
     }
 ]
