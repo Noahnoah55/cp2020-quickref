3b19682ed4139799f5c87ca50c300b13c2fc98f1
Noah Ehrman <giantnoah@gmail.com>
2020-09-12 16:56:00 -0400

    More melee rules

--- js/data_meleeaction.js
+++ js/data_meleeaction.js
@@ -47,12 +47,42 @@ data_meleeaction = [
 	{
 		title: "Block/Parry",
 		icon: "riot-shield",
-		subtitle: "Stop or absorb damage",
-		description: "Cast a spell with a casting time of 1 bonus action",
-		reference: "PHB, pg. 192.",
+		subtitle: "Stop or absorb melee damage",
+		description: "Stops incoming attacks at -3 to defender's other actions",
+		reference: "Core, pg. 112.",
 		bullets: [
-			"You can't cast a spell with you action and a different spell with your bonus action in the same turn, except if the action is used to cast a cantrip.",
-			"For further details, see the <i>Cast a spell</i> action."
+			"A player may elect to block/parry at the start of their turn",
+			"Any melee attacks made against the blocking/parrying character must first deal damage against the parrying object",
+			"(Unofficial) Shields will also block ranged attacks against them",
+			"Swords and other bladed weapons can be used to parry without taking damage, instead making a save (9 or lower) to avoid breaking",
+			"This counts as an action, and any other actions must be taken at a -3 penalty"
+		]
+	},
+	{
+		title: "Dodge",
+		icon: "dodging",
+		subtitle: "-2 to Attacker's hit roll",
+		description: "Makes it harder for an opponent to land a melee attack",
+		reference: "Core, pg. 112.",
+		bullets: [
+			"A player may elect to dodge at the start of their turn",
+			"Any melee attacks made against the dodging character are made at a -2 penalty",
+			"This counts as an action, and any other actions must be taken at a -3 penalty"
+		]
+	},
+	{
+		title: "Disarm",
+		icon: "fall-down",
+		subtitle: "Knock a weapon from opponent's hand",
+		description: "Melee attack to remove an opponent's weapon",
+		reference: "Core, pg. 111.",
+		bullets: [
+			"Attacker Rolls: Ref + Skill + Key Attack Bonus + 1d10",
+			"Defender Rolls: Ref + Skill + 1d10",
+			"Attacker uses their Martial Art (REF) or Brawling (REF)",
+			"Defender can use Martial arts, Fencing, Melee, Dodge, or Athletics depending on the situation and referee decision",
+			"If the attacker's roll is higher, the defender drops all weapons and items they are carrying in their hands",
+			"The attacker can then freely pick up the weapon, and fire it on their next turn"
 		]
 	}
 ]
